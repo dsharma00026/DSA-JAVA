53eed07f47698bbfeb898f66638260f1110c4d53
deepak sharma <dsharma00026@gmail.com>
2026-07-17 14:12:53 +0530

    for testing git iniliaztion

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main
 {
     public  static void main(String[] args)
     {
-        System.out.println("hello deepak this is first output program how are you");
+        System.out.println("msg  after git setup");
     }
 }
