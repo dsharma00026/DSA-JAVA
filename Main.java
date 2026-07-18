eb4c1a32128bdbc49c6b1a951472c25b8317d485
deepak sharma <dsharma00026@gmail.com>
2026-07-18 16:57:02 +0530

    learn about how to output in java also print n of * in patternr

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
-public class Main
-{
-    public  static void main(String[] args)
-    {
-        System.out.println("msg  after git setup");
+public class Main{
+    public static void main(String[] args){
+        System.out.println("hello world");
+        System.out.println("hello world");
+
+
+        //print a patttern
+        for(int i=0;i<5;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");        
+            }
+            System.out  .print("\n");
+        }
     }
 }
